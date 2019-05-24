2f7c2066f6e5309a1676096854a8c2651e521ccd
barbaraandrasi <andrasibarbara@yahoo.com>
2019-05-24 03:20:22 +0300

    FirstCommit

---
+++ Backend/MatchHistoryManager/MatchHistoryManager/Controllers/UserProfileController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MatchHistoryManager.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserProfileController : ControllerBase
+    {
+        private UserManager<IdentityUser> _userManager;
+        public UserProfileController(UserManager<IdentityUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        [HttpGet]
+        [Authorize]
+        //GET : /api/UserProfile
+        public async Task<Object> GetUserProfile()
+        {
+            string userId = User.Claims.First(c => c.Type == "UserID").Value;
+            var user = await _userManager.FindByIdAsync(userId);
+            return new
+            {
+                user.Email,
+                user.UserName
+            };
+        }
+    }
+}
